277daee5a62a513e1feaa9a237e1ca39f78a92c9
IoriBranford <ioribranford@gmail.com>
2023-03-03 14:34:53 -0800

    Use world in server. Host asset files via file server.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

--- server/main.go
+++ server/main.go
@@ -9,11 +9,20 @@ import (
 )
 
 func main() {
+	world := NewWorld()
+	world.LoadPlaces("assets/*.tmj")
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hi")
 	})
+	r.HandleFunc("/goto", func(w http.ResponseWriter, r *http.Request) {
+		world.handleGoTo(w, r)
+	})
+
+	fileServer := http.FileServer(http.Dir("assets/"))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
