c703f5960296fe5817164079e19b5ac57d7402e4
IoriBranford <ioribranford@gmail.com>
2023-03-03 00:33:53 -0800

    Test ReadPlace instead of LoadPlace.

--- server/Place_test.go
+++ server/Place_test.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadPlace(t *testing.T) {
-	place, err := LoadPlace("assets/Train_Day.tmj")
+func TestReadPlace(t *testing.T) {
+	placejson := `{
+		"layers": [
+			{
+				"image": "image.png"
+			},
+			{
+				"objects": [
+					{
+						"x": 10,
+						"y": 20,
+						"width": 30,
+						"height": 40,
+						"properties": [
+							{
+								"name": "NextPlace",
+								"value": "nextplace"
+							}
+						]
+					}
+				]
+			}
+		]
+	}`
+	place, err := ReadPlace(strings.NewReader(placejson))
 	assert.NotNil(t, place)
 	assert.Nil(t, err)
-	assert.Contains(t, place.image, "Train_Day")
+	assert.Equal(t, place.image, "image.png")
+	assert.Greater(t, len(place.exits), 0)
+	assert.Equal(t, place.exits[0].rect.x, 10)
+	assert.Equal(t, place.exits[0].rect.y, 20)
+	assert.Equal(t, place.exits[0].rect.width, 30)
+	assert.Equal(t, place.exits[0].rect.height, 40)
+	assert.Equal(t, place.exits[0].place, "nextplace")
 }
